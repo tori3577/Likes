da05787e65defbd457d675a2c6611d168433d930
Victoria Victorova <tori3577@gmail.com>
2015-11-08 13:22:16 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('velo').onclick = function() {imageChange()};
+
+
+function imageChange() {
+	var pic = document.getElementById('velo');
+
+     if (pic.src.match('Oldschool_bike')) {
+     	pic.src = 'image/Mountainbike.jpg';
+
+     }
+     else if (pic.src.match('Mountainbike')) {
+     	pic.src = 'image/Oldschool_basket.jpg';
+     }
+     else if (pic.src.match('Oldschool_basket')) {
+     	pic.src = 'image/Wall_bike.jpg';
+     }
+     else{
+     	pic.src ='image/Oldschool_bike.jpg';
+     }
+
+}
