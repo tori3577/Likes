31ce105ab8e78f7f52b5259c2781df97683f5f8f
Timur Yarosh <tyarosh@voiptime.net>
2015-11-09 23:26:11 +0200

    refactor

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
+window.onload = function () {
+    document.getElementById('velo').onclick = function () {
+        rotatePictures()
+    };
+    document.getElementById('clicks').onclick = function () {
+        setClick();
+    };
+};
 
+var pictures = [
+    'image/Mountainbike.jpg',
+    'image/Oldschool_basket.jpg',
+    'image/Wall_bike.jpg',
+    'image/Oldschool_bike.jpg'];
 
-document.getElementById('velo').onclick = function() {imageChange()};
-
-
-	function imageChange () {
-     var pic = document.getElementById('velo');
-
-     if (pic.src.match('Oldschool_bike')) {
-     	pic.src = 'image/Mountainbike.jpg';
-
-     }
-     else if (pic.src.match('Mountainbike')) {
-     	pic.src = 'image/Oldschool_basket.jpg';
-     }
-     else if (pic.src.match('Oldschool_basket')) {
-     	pic.src = 'image/Wall_bike.jpg';
-     }
-     else{
-     	pic.src ='image/Oldschool_bike.jpg';
-     }
-
+function rotatePictures() {
+    var pic = document.getElementById('velo');
+    // ищем индекс отображаемой картинки
+    var currentPictureIndex = pictures
+    // фильтруем массив с картинками,
+    // пропускаем дальше только те элементы, которые pic.src.match на которых вернет true
+        .filter(function (p) {
+            return pic.src.match(p)
+        })
+        // тут имеем массив только с одним найденным элементом благодаря filter
+        // преобразуем этот элемент в индекс этого элемента из массива
+        .map(function (p) {
+            return pictures.indexOf(p)
+        })
+        // тут имеем массив с одним элементом - индексом отображаемой картинки
+        // извлекаем этот элемент из полученного массива по индексу 0 и пишем в переменную currentPictureIndex
+        [0];
+    // объявляем индекс следующей картинки
+    var nextPictureIndex = 0;
+    // если текущая картинка не последняя в массиве
+    if (currentPictureIndex != (pictures.length - 1)) {
+        // то индекс следующей картинки будет индексом текущей + 1
+        nextPictureIndex = currentPictureIndex + 1;
+    }
+    // устанавливаем новую картинку
+    pic.src = pictures[nextPictureIndex];
 }
 
- var clicks = 0;
-    function setClick() {
-        clicks ++;
-        document.getElementById("clicks").innerHTML = clicks;
-    };
+var clicks = 0;
+function setClick() {
+    clicks++;
+    document.getElementById("clicks").innerHTML = clicks;
+};
