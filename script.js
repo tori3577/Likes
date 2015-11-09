1fc303a9db394b73eb77b76bcaf10de8cc9ce602
Timur Yarosh <tyarosh@voiptime.net>
2015-11-09 23:44:53 +0200

    refactor

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ window.onload = function () {
     };
 };
 
-var pictures = [
-    'image/Mountainbike.jpg',
-    'image/Oldschool_basket.jpg',
-    'image/Wall_bike.jpg',
-    'image/Oldschool_bike.jpg'];
+var pictures = {
+    'image/Mountainbike.jpg': 0,
+    'image/Oldschool_basket.jpg': 0,
+    'image/Wall_bike.jpg': 0,
+    'image/Oldschool_bike.jpg': 0
+};
 
 function rotatePictures() {
     var pic = document.getElementById('velo');
     // ищем индекс отображаемой картинки
-    var currentPictureIndex = pictures
+    // создаем массив из ключей pictures (image/Mountainbike.jpg, image/Oldschool_basket.jpg и т.д.)
+    var picturesArray = Object.keys(pictures);
+    var currentPictureIndex = picturesArray
     // фильтруем массив с картинками,
     // пропускаем дальше только те элементы, которые pic.src.match на которых вернет true
         .filter(function (p) {
@@ -25,7 +28,7 @@ function rotatePictures() {
         // тут имеем массив только с одним найденным элементом благодаря filter
         // преобразуем этот элемент в индекс этого элемента из массива
         .map(function (p) {
-            return pictures.indexOf(p)
+            return picturesArray.indexOf(p)
         })
         // тут имеем массив с одним элементом - индексом отображаемой картинки
         // извлекаем этот элемент из полученного массива по индексу 0 и пишем в переменную currentPictureIndex
@@ -33,12 +36,12 @@ function rotatePictures() {
     // объявляем индекс следующей картинки
     var nextPictureIndex = 0;
     // если текущая картинка не последняя в массиве
-    if (currentPictureIndex != (pictures.length - 1)) {
+    if (currentPictureIndex != (picturesArray.length - 1)) {
         // то индекс следующей картинки будет индексом текущей + 1
         nextPictureIndex = currentPictureIndex + 1;
     }
     // устанавливаем новую картинку
-    pic.src = pictures[nextPictureIndex];
+    pic.src = picturesArray[nextPictureIndex];
 }
 
 var clicks = 0;
