18fdcc5728b967de4a729909d4e68386a9017405
Timur Yarosh <tyarosh@voiptime.net>
2015-11-09 23:59:07 +0200

    add likes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
         rotatePictures()
     };
     document.getElementById('clicks').onclick = function () {
-        setClick();
+        like();
     };
 };
 
@@ -19,20 +19,7 @@ function rotatePictures() {
     // ищем индекс отображаемой картинки
     // создаем массив из ключей pictures (image/Mountainbike.jpg, image/Oldschool_basket.jpg и т.д.)
     var picturesArray = Object.keys(pictures);
-    var currentPictureIndex = picturesArray
-    // фильтруем массив с картинками,
-    // пропускаем дальше только те элементы, которые pic.src.match на которых вернет true
-        .filter(function (p) {
-            return pic.src.match(p)
-        })
-        // тут имеем массив только с одним найденным элементом благодаря filter
-        // преобразуем этот элемент в индекс этого элемента из массива
-        .map(function (p) {
-            return picturesArray.indexOf(p)
-        })
-        // тут имеем массив с одним элементом - индексом отображаемой картинки
-        // извлекаем этот элемент из полученного массива по индексу 0 и пишем в переменную currentPictureIndex
-        [0];
+    var currentPictureIndex = getCurrentPictureIndex(picturesArray, pic.src);
     // объявляем индекс следующей картинки
     var nextPictureIndex = 0;
     // если текущая картинка не последняя в массиве
@@ -41,11 +28,42 @@ function rotatePictures() {
         nextPictureIndex = currentPictureIndex + 1;
     }
     // устанавливаем новую картинку
-    pic.src = picturesArray[nextPictureIndex];
+    var nextPicture = picturesArray[nextPictureIndex];
+    pic.src = nextPicture;
+
+    // устанавливаем количество лайков новой картинки на кнопку
+    document.getElementById("clicks").innerText = pictures[nextPicture]
 }
 
-var clicks = 0;
-function setClick() {
-    clicks++;
-    document.getElementById("clicks").innerHTML = clicks;
-};
+// функция для поиска индекса отображаемой картинки
+function getCurrentPictureIndex(picturesArray, picSrc) {
+    return picturesArray
+    // фильтруем массив с картинками,
+    // пропускаем дальше только те элементы, picSrc.match() на которых вернет true
+        .filter(function (p) {
+            return picSrc.match(p)
+        })
+        // тут имеем массив только с одним найденным элементом благодаря filter
+        // преобразуем этот элемент в индекс этого элемента из массива
+        .map(function (p) {
+            return picturesArray.indexOf(p)
+        })
+        // тут имеем массив с одним элементом - индексом отображаемой картинки
+        // извлекаем этот элемент из полученного массива по индексу 0 и возвращаем из функции
+        [0];
+}
+
+function like() {
+    var pic = document.getElementById('velo');
+    // ищем индекс отображаемой картинки
+    // создаем массив из ключей pictures (image/Mountainbike.jpg, image/Oldschool_basket.jpg и т.д.)
+    var picturesArray = Object.keys(pictures);
+    // получаем индекс текущей картинки, вызвав функцию, которая его ищет
+    var currentPictureIndex = getCurrentPictureIndex(picturesArray, pic.src);
+    // получаем текущую картинку, обращением к элементу массива по индексу
+    var currentPicture = picturesArray[currentPictureIndex];
+    // увеличиваем количество лайков на 1
+    pictures[currentPicture]++;
+    // устанавливаем количество лайков на кнопку
+    document.getElementById("clicks").innerText = pictures[currentPicture];
+}
